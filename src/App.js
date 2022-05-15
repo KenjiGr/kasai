3d343a25f12540305685c9d1cfbc9b4005c25f01
KenjiGr <kenjigrr@gmail.com>
2022-05-14 20:00:43 -0400

    credentials initialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import {useEffect} from 'react'
+import Credentials from './utils/Credentials';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios.post()
+  },[])
+
   return (
     <div>
 
---
+++ src/utils/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+    return {
+        ClientId: 'b1e634e57fbd4aa3aa83b8c09f35f8b5',
+        ClientSecret: '',
+    }
+}
+
+export default Credentials
